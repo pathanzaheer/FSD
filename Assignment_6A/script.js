f09025b5edb930418cab505c335d9016e4f3f7e0
pathanzaheer <zaheerpatan56@gmail.com>
2025-02-08 11:28:08 +0530

    Update script.js

--- Assignment_6A/script.js
+++ Assignment_6A/script.js
@@ -1,3 +1,3 @@
 function changeTextP2(){
-    document.getElementById("para-2").innerHTML="This text is changed using external javascript"
+    document.getElementById("para-2").innerHTML="This text is updated using external javascript"
 }
