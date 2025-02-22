6d30dd13bc630c484841edcb5e1683478cbc1c98
pathanzaheer <zaheerpatan56@gmail.com>
2025-02-22 11:59:33 +0530

    Add files via upload

---
+++ Assignment_6B/script.js
@@ -0,0 +1,15 @@
+function function1(){
+    alert("This ia an alert box");
+}
+function function2(){
+    document.open();
+    document.write("This text is written directly to the document");
+    document.close();
+}
+function function3(){
+    console.log("This method is logged to the console.");
+    
+}
+function function4(){
+    document.getElementById("op").innerHTML = "This text is updated using inner HTML"
+}
